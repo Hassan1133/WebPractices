c35f96ea99595ff6e7d971d2028011bc5f2c0dbe
Hassan1133 <mhassanidrees575@gmail.com>
2022-08-22 13:53:13 +0500

    bootstrap simple

---
+++ web.js
@@ -0,0 +1,7 @@
+var age = +prompt("enter your age");
+// var gender = +prompt("enter your gender");
+if (age >= 18) {
+    alert("You are allowed to drive");
+} else {
+    alert("You are not allowed to drive ");
+}
